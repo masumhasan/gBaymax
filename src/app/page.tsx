97ab8857a815438a9ab1f74b33655390380c0326
Md. Nur Hasan Masum <masumhasan92@gmail.com>
2025-07-24 22:40:33 +0000

    yoyo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,33 +51,32 @@ export default function Home() {
     }
     return <Room token={token} serverUrl={serverUrl} />;
   }
-
   return (
-      <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-background">
-        <div className="flex flex-col items-center" style={{ marginTop: '-68px' }}>
-            <Image
-                src="https://gameghor.github.io/public/baymax.gif"
-                alt="Baymax"
-                width={400}
-                height={300}
-                unoptimized
-            />
-            <Card className="w-full max-w-md shadow-lg border-2 border-primary/20 -mt-20 z-10">
-              <CardHeader>
-                <CardTitle className="text-3xl font-headline text-center text-primary-foreground bg-primary -mx-6 px-6 py-4 rounded-t-lg -mt-6">gBaymax</CardTitle>
-                <CardDescription className="text-center pt-4">
-                  Start a session with your personal healthcare companion.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="flex justify-center">
-                  <Button onClick={handleJoinRoom} className="w-full bg-accent hover:bg-accent/90 text-accent-foreground" disabled={isLoading}>
-                    {isLoading ? <Loader2 className="animate-spin" /> : "Start Session"}
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
-        </div>
-      </main>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 pt-20 bg-background">
+      <div className="flex flex-col items-center">
+          <Image 
+              src="https://gameghor.github.io/public/baymax.gif"
+              alt="Baymax"
+              width={400}
+              height={300}
+              unoptimized
+          />
+          <Card className="w-full max-w-md shadow-lg border-2 border-primary/20 -mb-50 z-10">
+            <CardHeader>
+              <CardTitle className="text-3xl font-headline text-center text-primary-foreground bg-primary -mx-6 px-6 py-4 rounded-t-lg -mt-6">gBaymax</CardTitle>
+              <CardDescription className="text-center pt-4">
+                Start a session with your personal healthcare companion.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex justify-center">
+                <Button onClick={handleJoinRoom} className="w-full bg-accent hover:bg-accent/90 text-accent-foreground" disabled={isLoading}>
+                  {isLoading ? <Loader2 className="animate-spin" /> : "Start Session"}
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+      </div>
+    </main>
   );
-}
+  
